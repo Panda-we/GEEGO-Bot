f009127c0ca4fd2349188f421abba0ccffe0719d
Panda-we <panda-we@users.noreply.github.com>
2025-11-01 18:35:45 +0530

    Focus: normalize messages, update ChatMessage API, add scroll-to-bottom

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://geego-bot.onrender.com/api',
+  baseURL: 'http://localhost:3000/api',
   withCredentials: true,
 })
 
